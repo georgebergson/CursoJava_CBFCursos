04c62952b8bb91c31ab19771792fc4330bbc194f
George <george_bergson@hotmail.com>
2023-08-30 06:55:28 -0400

    Entendendo variaveis e impressao com Formataçao

---
+++ Aula02/Aula02.java
@@ -0,0 +1,12 @@
+package Aula02;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        int valor_1 =20, valor_2 = 30, valor_3 = 50;
+        float resultado = valor_1 + valor_2 + valor_3;
+        String nome = "George Bergson";
+
+        System.out.println("Olá meu nome é: "+nome);
+        System.out.printf("%s %f","O resultado da soma é:", resultado);
+    }
+}
